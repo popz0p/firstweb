d41b9041b8899741453eedbe6a401f656a8886fb
popz0p <yerdenelectric@gmail.com>
2023-03-10 12:51:43 -0500

    Renamed VS code files for Vue Course

---
+++ app.js
@@ -0,0 +1,10 @@
+const buttonEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.querySelector('ul');
+
+function addGoal() {
+    const enteredValue = inputel.value;
+    const listEl = document.createElement('li');
+    listItemeEl.textContent = enteredvalue;
+    listEl.append 
+}
