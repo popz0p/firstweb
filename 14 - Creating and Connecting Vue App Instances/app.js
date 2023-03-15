e04643c68b63e7580c6df8fca6bf6ef0cd9088ac
popz0p <yerdenelectric@gmail.com>
2023-03-15 16:50:47 -0400

    Vue lesson thru lesson 17

--- 14 - Creating and Connecting Vue App Instances/app.js
+++ 14 - Creating and Connecting Vue App Instances/app.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn Vue!',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue!';
+              } else {
+                return 'Master Vue!';
+            }
+        },
     }
-});
+ });
 
 app.mount('#user-goal');
